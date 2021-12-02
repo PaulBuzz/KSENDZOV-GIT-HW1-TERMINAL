97bf3894e4a35314d6cc7f6c33f1c46008993229
Pavel Buzin <84155505+PaulBuzz@users.noreply.github.com>
2021-12-02 12:46:06 +0200

    Delete homework-1-advanced-2.sh

--- HW1/homework-1-advanced-2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd /Users/macintosh/Desktop/CODING/KSENDZ/SCRPT
-mkdir -p f_0 / f_1 / f_2
-cd /Users/macintosh/Desktop/CODING/KSENDZ/SCRPT/f_0
-touch f0.txt f1.txt f2.txt j0.json j1.json
-mkdir -p fn_0 / fn_1 / fn_2
-ls -la
-mv f0.txt f1.txt fn_0
